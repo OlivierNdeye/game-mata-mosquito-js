e1ab1d4a71cdcb6adf7e531d0dc8c49f030bc725
OlivierNdeye <oliviersndeye@gmail.com>
2024-09-29 17:09:29 -0300

    inserindo os arquivos de imagem, configurando o ajuste de janela do app do jogo

---
+++ jogo.js
@@ -0,0 +1,13 @@
+var altura = 0;
+var largura = 0;
+
+
+function ajustaTamPalcoJogo() {
+    var altura = window.innerHeight;
+    var largura = window.innerWidth;
+    console.log(altura, largura);
+}
+
+ajustaTamPalcoJogo()
+
+
